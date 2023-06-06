73f69d4119347b12b9c6c716111432b543b50922
Yenleng <yenlengggggg@gmail.com>
2023-06-06 15:03:02 +0800

    add changes

--- src/main/java/com/example/cryptocompareapi/controller/NewsController.java
+++ src/main/java/com/example/cryptocompareapi/controller/NewsController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,23 +38,12 @@ public class NewsController {
     @PostMapping(path="/articles")
     public String saveArticles(@ModelAttribute("savedItems") SavedItems savedItems, Model m) throws IOException{
         List<String> selectedNews = savedItems.getSelectedItems();
-        List<String> selectedItems = new ArrayList<>();
-    
-        // for (SavedItems.NewsInfo newsInfo : selectedNews) {
-        //     String item = newsInfo.getId() + "," +
-        //                   newsInfo.getPublished_on() + "," +
-        //                   newsInfo.getTitle() + "," +
-        //                   newsInfo.getUrl() + "," +
-        //                   newsInfo.getImageURL() + "," +
-        //                   newsInfo.getBody() + "," +
-        //                   newsInfo.getTags() + "," +
-        //                   newsInfo.getCategories();
-    
-        //     selectedItems.add(item);
-        // }
-        savedItems.setSelectedItems(selectedItems);
-        m.addAttribute("savedItems", savedItems);
+        selectedNews.removeIf(Objects::isNull);
+
+        m.addAttribute("savedItems", selectedNews);
         System.out.println("selected news-----> " + selectedNews);
+
+
 
         return "articles";
     }
--- src/main/java/com/example/cryptocompareapi/model/SavedItems.java
+++ src/main/java/com/example/cryptocompareapi/model/SavedItems.java
@@ -1,8 +1,6 @@
 package com.example.cryptocompareapi.model;
 
 import java.io.Serializable;
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SavedItems implements Serializable {
@@ -14,75 +12,8 @@ public class SavedItems implements Serializable {
     public void setSelectedItems(List<String> selectedItems) {
         this.selectedItems = selectedItems;
     }
+    
 
-    
-    public static class NewsInfo {
-        private String id;
-        private Long published_on;
-        private String title;
-        private String url;
-        private String imageURL;
-        private String body;
-        private String tags;
-        private String categories;
-        public String getId() {
-            return id;
-        }
-        public void setId(String id) {
-            this.id = id;
-        }
-        public Long getPublished_on() {
-            return published_on;
-        }
-        public void setPublished_on(Long published_on) {
-            this.published_on = published_on;
-        }
-        public String getTitle() {
-            return title;
-        }
-        public void setTitle(String title) {
-            this.title = title;
-        }
-        public String getUrl() {
-            return url;
-        }
-        public void setUrl(String url) {
-            this.url = url;
-        }
-        public String getImageURL() {
-            return imageURL;
-        }
-        public void setImageURL(String imageURL) {
-            this.imageURL = imageURL;
-        }
-        public String getBody() {
-            return body;
-        }
-        public void setBody(String body) {
-            this.body = body;
-        }
-        public String getTags() {
-            return tags;
-        }
-        public void setTags(String tags) {
-            this.tags = tags;
-        }
-        public String getCategories() {
-            return categories;
-        }
-        public void setCategories(String categories) {
-            this.categories = categories;
-        }
-        @Override
-        public String toString() {
-            return "NewsInfo [id=" + id + ", published_on=" + published_on + ", title=" + title + ", url=" + url
-                    + ", imageURL=" + imageURL + ", body=" + body + ", tags=" + tags + ", categories=" + categories
-                    + "]";
-        }
-       
-        
-    
-    
-    }
+}  
 
-}
+
